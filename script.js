4e3d2fddcda747d29538f66d3a85f1b4217a74bc
jeremycfwong <71006547+jeremycfwong@users.noreply.github.com>
2023-01-06 01:39:36 +0800

    Add Image Slider Base
    As title

--- script.js
+++ script.js
@@ -17,3 +17,38 @@ function toggleHidden(element) {
     }
 }
 
+
+var swiperPrevButton = document.getElementById('swiper-prev')
+swiperPrevButton.addEventListener('click', (_) => {
+    swiperButtonLogic('previous')
+})
+var swiperNextButton = document.getElementById('swiper-next')
+swiperNextButton.addEventListener('click', (_) => {
+    swiperButtonLogic('next')
+})
+
+function swiperButtonLogic(action){
+    var imageArray = document.getElementsByClassName('img-swiper-item');
+    var target = 0;
+    for (var i = 0; i < imageArray.length; i++){
+        if (!imageArray[i].classList.contains('hidden')){
+            imageArray[i].classList.add('hidden')
+            if(action == 'previous'){
+                if(i == 0){
+                    target = 3
+                } else{
+                    target = i - 1
+                }
+            } else {
+                if(i == 3){
+                    target = 0
+                } else{
+                    target = i + 1
+                }
+            }
+            
+            break
+        }
+    }
+    imageArray[target].classList.remove('hidden')
+}
